1b1108a428ab3fcbac752864c89dc9c39973aff7
Rauf <kurdrauf74@gmail.com>
2026-01-01 20:36:35 +0300

    feat: implement hamburger menu.

--- assets/scripts/script.js
+++ assets/scripts/script.js
@@ -1,4 +1,32 @@
 // Toggle close & Open hamburger Menu
+const menuEl = document.getElementById("mobile-screen-menu");
+const menuBtnEl = document.getElementById("menu-btn");
+const headerEl = document.getElementById("header");
+
+// close menu when click outside of menu
+document.querySelector("body").addEventListener("click", (e) => {
+  //   console.log(
+  //     e.target.parentElement,
+  //     ":",
+  //     e.target.parentElement.classList.contains("menu")
+  //   );
+  if (e.target.parentElement.classList.contains("menu")) {
+    menuEl.classList.toggle("translate-x-full");
+    menuButtonType();
+  } else {
+    menuEl.classList.add("translate-x-full");
+    menuButtonType();
+  }
+});
+function menuButtonType() {
+  if (menuEl.classList.contains("translate-x-full")) {
+    menuBtnEl.innerHTML =
+      '<i class="bx bx-menu bx-rotate-90 bx-flip-vertical" style="color: #ffffff" ></i>';
+  } else {
+    menuBtnEl.innerHTML = "<i class='bx  bx-x' style='color:#ffffff'></i>";
+  }
+}
+
 // Auto get Year for Footer
 const copyrightEl = document.getElementById("year");
 copyrightEl.textContent = new Date().getFullYear();
